71dc2b33d2f5885cca8397aba916a72f302ef094
Andrew Henshaw <andrew@henshaw.us>
2019-03-01 11:16:09 -0500

    Referenced RFC1323 to explain the use of 2^31 in the comparison.

--- src/tcp.rs
+++ src/tcp.rs
@@ -311,7 +311,14 @@ impl Connection {
 }
 
 fn wrapping_lt(lhs:u32, rhs: u32) -> bool {
-    lhs.wrapping_sub(rhs) > u32::max_value() >> 1 
+    // From RFC1323:
+    //     TCP determines if a data segment is "old" or "new" by testing
+    //     whether its sequence number is within 2**31 bytes of the left edge
+    //     of the window, and if it is not, discarding the data as "old".  To
+    //     insure that new data is never mistakenly considered old and vice-
+    //     versa, the left edge of the sender's window has to be at most
+    //     2**31 away from the right edge of the receiver's window.
+    lhs.wrapping_sub(rhs) > 2^31 
 }
 
 fn is_between_wrapped(start: u32, x: u32, end: u32) -> bool {
